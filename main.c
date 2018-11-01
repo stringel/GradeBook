2dcc51e611a61a940d7c5d1eef3ff76e33c48994
Stringer <stringel@ohio.edu>
2018-11-01 19:16:40 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Grader.h"
+
+int main()
+{
+
+    //float data[MAX_GRADES] = {78.8F,99.3F,105.F,89.9F,37.F};
+    float data[MAX_GRADES];
+    int i = 0;
+
+    while (i < MAX_GRADES)
+    {
+        printf("Enter a grade:" );
+
+        float grade;
+        scanf("%f", &grade);
+
+        data[i] = grade;
+
+        i++;
+    }
+
+    assignShowGrade(data);
+
+
+    return 0;
+}
